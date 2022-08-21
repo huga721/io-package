c6a38cf85c15ee596dd522a4fd534021c11c2fe2
hubert <szabahubert@gmail.com>
2022-08-21 10:55:55 +0200

    adding to repo

---
+++ src/com/timbuchalka/Lokacje.java
@@ -0,0 +1,132 @@
+package com.timbuchalka;
+
+import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Lokacje implements Map<Integer, Location> {
+    private static final Map<Integer, Location> locations = new LinkedHashMap<>();
+
+    // Writing the object to the "locations.dat" file and then reading object from the file and putting values to map and so on
+    public static void main(String[] args) throws IOException{
+        // Creating and instance of a path
+        Path locPath = FileSystems.getDefault().getPath("locations.dat");
+        // Opening a ObjectOutputStream, and first we create instance of BufferedOutputStream so object will be buffered
+        // Then we pass the path
+        try (ObjectOutputStream locFile = new ObjectOutputStream(new BufferedOutputStream(Files.newOutputStream(locPath)))){
+            // Looping the map value to write every object
+            for (Location loc : locations.values()){
+                locFile.writeObject(loc); // writing object to "locations.dat"
+            }
+        } catch (IOException e) {
+            System.out.println("IOException in main");
+        }
+
+    }
+    static {
+        // Instance of locations.dat path
+        Path objPath = FileSystems.getDefault().getPath("locations.dat");
+        // Creating instance of ObjectInputStream, reading object in BufferedInputStream and passing path of the file that contains object
+        try (ObjectInputStream objFile = new ObjectInputStream(new BufferedInputStream(Files.newInputStream(objPath)))){
+            boolean eof = false;
+            while (!eof){
+                try {
+                    Location location = (Location)objFile.readObject(); // New instance of Location class, read from the file
+                    locations.put(location.getLocationID(), location); // Putting object to the map
+                } catch (EOFException e){
+                    eof = true;
+                } catch (ClassNotFoundException e){
+                    e.printStackTrace();
+                }
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Creating BufferedReader objects to read data from the files
+//        try(BufferedReader locFile = new BufferedReader(new FileReader("locations_big.txt"));
+//            BufferedReader dirFile = new BufferedReader(new FileReader("directions_big.txt"))){
+//
+//            String locString;
+//            String dirString;
+//            while ((locString = locFile.readLine()) != null){ // while the line that has been read is not null
+//                String[] split = locString.split(","); // Split the line and put it to the array
+//                int id = Integer.parseInt(split[0]);
+//                String description = split[1];
+//                locations.put(id, new Location(id, description, null));
+//            }
+//            while ((dirString = dirFile.readLine()) != null){
+//                String[] split = dirString.split(",");
+//                int id = Integer.parseInt(split[0]);
+//                String direction = split[1];
+//                int destination = Integer.parseInt(split[2]);
+//                locations.get(id).addExit(direction, destination);
+//            }
+//
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
+    }
+
+    @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+}
---
+++ src/com/timbuchalka/Main.java
@@ -0,0 +1,69 @@
+package com.timbuchalka;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * For learning purpose
+ */
+public class Main {
+    private static Lokacje lokacje = new Lokacje();
+
+    public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+
+        Map<String, String> vocabulary = new LinkedHashMap<>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+
+        // Starting location
+        int loc = 1;
+        while(true) { // infinite loop
+            System.out.println(lokacje.get(loc).getDescription()); // Printing description of starting location
+            // if location == 0 then break the loop
+            if(loc == 0) {
+                break;
+            }
+
+            // Reference to get the exits from current location
+            Map<String, Integer> exits = lokacje.get(loc).getExits();
+            System.out.print("Available exits are ");
+            for(String exit: exits.keySet()) {
+                System.out.print(exit + ", "); // printing current locations available
+            }
+            System.out.println();
+
+            // Blocking method, to receive the direction input, where you want to go
+            String direction = scanner.nextLine().toUpperCase();
+
+            // If the input is more than one character, then it looks for the correct word in vocabulary map, that contains correct words
+            if(direction.length() > 1) {
+                String[] words = direction.split(" "); // Splitting the input to single words array
+
+                // Looping through map to check for the input
+                for(String word: words) {
+                    // If map contains input
+                    if(vocabulary.containsKey(word)) {
+                        // Then direction is equal to value of the key
+                        direction = vocabulary.get(word);
+                        System.out.println(vocabulary.get(word)); // Testing
+                        break;
+                    }
+                }
+            }
+            // If exits map contains direction key
+            if(exits.containsKey(direction)) {
+
+                // Then loc variable is equal to id from the exits map of the location that we choose to go
+                loc = exits.get(direction);
+
+            } else {
+                System.out.println("You cannot go in that direction");
+            }
+        }
+    }
+}
